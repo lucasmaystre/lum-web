8512138bf7e7d723a4f4e384a9e2c814bc8cfc97
Lucas Maystre <lucas@maystre.ch>
2012-08-19 12:40:56 +0200

    Added shortcut to referee assignments.
    Implemented as a temporary redirection (302) when hitting the /arb endpoint.

--- homepage.py
+++ homepage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 
 app = Flask(__name__)
@@ -13,6 +13,12 @@ def homepage():
 @app.route('/projects')
 def projects():
     return render_template('projects.html')
+
+
+@app.route('/arb')
+def ref_assignments():
+    return redirect('http://www.football.ch/fr/SFV/SFV-Service/Schiedsrichter/'
+                    'Recherche-darbitres.aspx/v-1191/sr-588481/')
 
 
 if __name__ == '__main__':
