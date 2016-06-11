9c538091ff0f6d0b0738d1856a32d4a9ad07a071
Lucas Maystre <lucas@maystre.ch>
2016-06-11 20:51:08 +0100

    Added kickoff.ai score update wizard.
    Temporary solution to update the score of Euro 2016 games on Kickoff.ai. Will
    need to eventually move to kickoff.ai.

--- homepage.py
+++ homepage.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
-from flask import Flask, render_template, redirect
+import json
+import requests
+
+from flask import Flask, render_template, redirect, request
 
 
 app = Flask(__name__)
@@ -28,6 +31,20 @@ def ref_assignments():
 @app.route('/nips15')
 def nips15():
     return render_template('nips15.html', home_link=True)
+
+@app.route('/kickoffai', methods=['GET', 'POST'])
+def kickoff_ai():
+    url = "https://nqwxwb9jr2.execute-api.eu-central-1.amazonaws.com/beta/game"
+    res=None
+    if request.form.get('submitted'):
+        payload = {
+            'gameId': int(request.form['gameId']),
+            'scoreHome': int(request.form['scoreHome']),
+            'scoreAway': int(request.form['scoreAway']),
+        }
+        headers = {'x-api-key': request.form['apiKey']}
+        res = requests.post(url, headers=headers, data=json.dumps(payload))
+    return render_template('kickoffai.html', res=res)
 
 
 if __name__ == '__main__':
